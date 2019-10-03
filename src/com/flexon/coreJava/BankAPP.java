b3134e1ac3f0a140df1ef0c38f9c9d9d5782b5b0
zijianren <zijianren0918@gmail.com>
2019-10-03 13:52:32 -0700

    commit

--- src/com/flexon/coreJava/BankAPP.java
+++ src/com/flexon/coreJava/BankAPP.java
@@ -1,5 +1,10 @@
 package com.flexon.coreJava;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class BankAPP {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,6 +19,11 @@ class BankAccount{
 	String customerName;
 	String email;
 	String phoneNumber;
+	double remainingDepositAmount = 1500;
+	double remainingWithdrawAmount = 1000;
+	int remainingDepositNumber = 3;
+	LocalDate date = LocalDate.now();
+	ArrayList<String> history = new ArrayList<>();
 	public BankAccount(int accountNumber, double balance, String customerName, String email, String phoneNumber) {
 		this.accountNumber = accountNumber;
 		this.balance = balance;
@@ -23,17 +33,41 @@ class BankAccount{
 	}
 	
 	public void deposit(double funds) {
+		getDate();
+		if (funds < 50 || funds > 500) {
+			System.out.println("Operation failed");
+			System.out.println("Each deposit should be greater than (including) $ 50 and less than (including) $500");
+			return;
+		}
+		
+		if (remainingDepositNumber <= 0 || remainingDepositAmount < funds) {
+			System.out.println("Operation failed");
+			System.out.println("Exceed the daily deposit limit");
+			return;
+		}
+		
 		this.balance = this.balance + funds;
+		remainingDepositAmount = remainingDepositAmount - funds;
+		remainingDepositNumber = remainingDepositNumber - 1;
+		history.add("Deposit: $" + funds);
 		System.out.println("Operation succeed");
+		System.out.println("You have deposited $" + funds);
 		System.out.println("Your balance is $" + this.balance);
 		return;
 	}
 	
 	public void withdraw(double funds) {
-		if (funds <= this.balance) {
+		getDate();
+		System.out.println("The money you want to withdraw is $" + funds);
+		
+		if (funds <= this.balance && remainingWithdrawAmount >= funds) {
 			this.balance = this.balance - funds;
+			remainingWithdrawAmount = remainingWithdrawAmount - funds;
+			history.add("Withdraw: $" + funds);
 			System.out.println("Operation succeed");
+
 			System.out.println("Your balance is $" + this.balance);
+			System.out.println("Your remaining withdrawal today is $" + remainingWithdrawAmount);
 		} else {
 			System.out.println("Operation failed");
 			System.out.println("Your balance is $" + this.balance);
@@ -42,11 +76,37 @@ class BankAccount{
 	}
 	
 	public void printInformation() {
-		System.out.println(this.customerName);
-		System.out.println(this.accountNumber);
-		System.out.println(this.balance);
-		System.out.println(this.email);
-		System.out.println(this.phoneNumber);
+		System.out.println("Name: " + this.customerName);
+		System.out.println("Account Number: " + this.accountNumber);
+		System.out.println("Email: " + this.email);
+		System.out.println("Phone Number: " + this.phoneNumber);
+		return;
+	}
+	
+	public void showBalance() {
+		System.out.println("Name: " + this.customerName);
+		System.out.println("Your balance is: $" + this.balance);
+		Date t = new Date();
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(df.format(t));
+	}
+	
+	private void getDate() {
+		LocalDate today = LocalDate.now();
+		if (!today.equals(date)) {
+			remainingDepositAmount = 1500;
+			remainingDepositNumber = 3;
+			remainingWithdrawAmount = 1000;
+			date = today;
+			history = new ArrayList<>();
+		}
+		return;
+	}
+	
+	public void transactionHistory() {
+		for (String s : history) {
+			System.out.println(s);
+		}
 		return;
 	}
 }
--- src/com/flexon/coreJava/BankOperation.java
+++ src/com/flexon/coreJava/BankOperation.java
@@ -10,26 +10,26 @@ public class BankOperation {
 		
 		while (true) {
 			System.out.println("Please select your operation: 1. check your balance 2. deposit money "
-					+ "3. withdraw money 4. print your information 5. exit");
+					+ "3. withdraw money 4. print your information 5. check transaction history 6. exit");
 			Scanner scan = new Scanner(System.in);
 			int input = scan.nextInt();
 			if (input == 1) {
-				System.out.println("Your balance is $" + jay.balance);
+				jay.showBalance();
 			} else if (input == 2) {
 				System.out.println("Please insert the amount of money you want to deposit:");
 				Scanner deposit = new Scanner(System.in);
-				double depositMoney = deposit.nextDouble();
-				System.out.println("You have deposited $" + depositMoney);	
+				double depositMoney = deposit.nextDouble();	
 				jay.deposit(depositMoney);
 			} else if (input == 3) {
 				System.out.println("Please insert the amount of money you want to withdraw:");
 				Scanner withdraw = new Scanner(System.in);
 				double withdrawMoney = withdraw.nextDouble();
-				System.out.println("The money you want to withdraw is $" + withdrawMoney);
 				jay.withdraw(withdrawMoney);
 			} else if (input == 4) {
 				jay.printInformation();
-			} else {
+			} else if (input == 5) {
+				jay.transactionHistory();
+			}else {
 				System.out.println("Thank you!");
 				break;
 			}
